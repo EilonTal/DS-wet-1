6f6bb4cd38a8e3571e8d62f124f3594a03a9ee05
Eilon Tal <eilontal11@gmail.com>
2020-12-01 11:26:57 +0200

    started rolls and some functions
    needs to add conditions to rolls, for instance : what if right tree is nullptr or right left, and so on.

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -1,7 +1,7 @@
 #include "AVLTree.h"
 template<class T>
 AVLTree<T>::AVLTree(T& data):
-    : data(data), first(this), hight(1)
+: data(data), first(this), height(1), father(nullptr)
 {
 }
 
@@ -9,7 +9,7 @@ template<class T>
 StatusType AVLTree<T>::insertElement(T &element)
 {
     StatusType return_value = SUCCESS;
-    if (data == element) 
+    if (data == element)
     {
         return FAILURE;
     }
@@ -29,9 +29,10 @@ template<class T>
 StatusType AVLTree<T>::insertToLeft(T& element)
 {
     StatusType return_value = SUCCESS;
-    if (left_tree == nullptr) 
+    if (left_tree == nullptr)
     {
-        left_tree = std::shared_ptr<AVLTree>(AVLTree(element));   
+        left_tree = std::shared_ptr<AVLTree>(AVLTree(element));
+        left_tree->father = this;
     }
     else
     {
@@ -44,9 +45,11 @@ template<class T>
 StatusType AVLTree<T>::insertToRight(T& element)
 {
     StatusType return_value = SUCCESS;
-    if (right_tree == nullptr) 
+    if (right_tree == nullptr)
     {
-        right_tree = std::shared_ptr<AVLTree>(AVLTree(element));   
+        right_tree = std::shared_ptr<AVLTree>(AVLTree(element));
+        right_tree->father = this;
+
     }
     else
     {
@@ -56,43 +59,43 @@ StatusType AVLTree<T>::insertToRight(T& element)
 }
 
 template<class T>
-int AVLTree<T>::getLeftHight()
+int AVLTree<T>::getLeftHeight()
 {
     if (left_tree == nullptr)
     {
         return 0;
     }
-    return left_tree.calculateHight();
+    return left_tree.calculateHeight();
 }
 
 template<class T>
-int AVLTree<T>::getRightHight()
+int AVLTree<T>::getRightHeight()
 {
     if (right_tree == nullptr)
     {
         return 0;
     }
-    return right_tree.calculateHight();
+    return right_tree.calculateHeight();
 }
 
 template<class T>
-int AVLTree<T>::calculateHight()
+int AVLTree<T>::calculateHeight()
 {
-    return 1+getRightHight()+getLeftHight();
+    return 1 + std::max(getRightHeight(), getLeftHeight());
 }
 
 template<class T>
-int AVLTree<T>::calculateBalance() 
+int AVLTree<T>::calculateBalance()
 {
-    return getLeftHight()-getRightHight();
+    return getLeftHeight()-getRightHeight();
 }
 
 template<class T>
-void AVLTree<T>::doRoll() 
+void AVLTree<T>::doRoll()
 {
-    hight = calculateHight();
+    height = calculateHeight();
     balance = calculateBalance();
-    if (balance > 1) 
+    if (balance > 1)
     {
         if (left_tree->balance >= 0)
         {
@@ -108,14 +111,74 @@ void AVLTree<T>::doRoll()
         }
         RL();
     }
-    hight = calculateHight();
-    balance = calculateBalance();   
+    height = calculateHeight();
+    balance = calculateBalance();
 }
+
 template<class T>
 void AVLTree<T>::LL()
 {
-    std::shared_ptr<AVLTree> temp = this;
-    
+    std::shared_ptr<AVLTree> AR_temp = this->left_tree->right_tree;
+
+    // changing A to be root
+    this = this->left_tree;
+
+    // changing A right son to be B
+    this->right_tree = this->father;
+    this->right_tree->father = this;
+    this->father = nullptr;
+
+    // rolling previous right son of A to be left son of B
+    this->right_tree->left_tree = AR_temp;
+    this->right_tree->left_tree->father = this->right_tree;
+
+    //update height and balance
+    this->right_tree->height = this->right_tree->calculateHeight();
+    this->right_tree->balance = this->right_tree->calculateBalance();
+    height = calculateHeight();
+    balance = calculateBalance();
+}
+
+template<class T>
+void AVLTree<T>::RR()
+{
+    std::shared_ptr<AVLTree> BL_temp = this->right_tree->left_tree;
+
+    // changing A to be root
+    this = this->right_tree;
+
+    // changing A left son to be B
+    this->left_tree = this->father;
+    this->left_tree->father = this;
+    this->father = nullptr;
+
+    // rolling previous left son of A to be left son of B
+    this->left_tree->right_tree = BL_temp;
+    this->left_tree->right_tree->father = this->left_tree;
+
+    //update height and balance
+    this->right_tree->height = this->right_tree->calculateHeight();
+    this->right_tree->balance = this->right_tree->calculateBalance();
+    height = calculateHeight();
+    balance = calculateBalance();
+}
+
+template<class T>
+void AVLTree<T>::LR()
+{
+    std::shared_ptr<AVLTree> A_temp = this->right_tree->left_tree;
+    this->left_tree->RR();
+    A_temp->father = this;
+    this->LL();
+}
+
+template<class T>
+void AVLTree<T>::RL()
+{
+    std::shared_ptr<AVLTree> A_temp = this->right_tree->left_tree;
+    this->right_tree->LL();
+    A_temp->father = this;
+    this->RR();
 }
 
 template<class T>
@@ -130,11 +193,45 @@ StatusType AVLTree<T>::deleteThisElement(std::shared_ptr<AVLTree> v)
 {
     if (isLeaf)
     {
-        if (father->left_tree == v) 
+        if (father->left_tree == v)
         {
-            
+
         }
     }
+}
+
+bool AVLTree::isLeaf()
+{
+    return right_tree == nullptr && left_tree == nullptr;
+}
+
+bool AVLTree::isRoot()
+{
+    return father == nullptr;
+}
+
+template<class T>
+std::shared_ptr<AVLTree<T>> AVLtree::getLeftTree()
+{
+    return left_tree;
+}
+
+template<class T>
+std::shared_ptr<AVLTree<T>> AVLtree::getRightTree()
+{
+    return right_tree;
+}
+
+template<class T>
+std::shared_ptr<AVLTree<T>> AVLtree::getFather()
+{
+    return father;
+}
+
+template<class T>
+std::shared_ptr<AVLTree<T>> AVLtree::getFirst()
+{
+    return first;
 }
 
 template<class T>
